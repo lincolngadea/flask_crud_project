1b803d938187e75930c4a932190b7d0683c50937
lincolngadea <lincolngadea@gmail.com>
2025-01-07 07:45:22 -0300

    chore: implementa o endpoint para buscar usuário por id

--- app/controllers/UserController.py
+++ app/controllers/UserController.py
@@ -21,5 +21,19 @@ def store():
         return Response(json.dumps([user.serialize()]))
     except Exception as e:
         session.rollback()
+        return Response(json.dumps({"Erro":f"Não foi possível criar o usuário - Erro: {e}"}),
+                        status=500, mimetype='application/json')
+    finally:
+        session.close()
+
+def show(user_id):
+    session = db.session()
+    try:
+        user = session.query(User).get(user_id)
+        return Response(json.dumps([user.serialize()]))
+    except Exception as e:
+        session.rollback()
+        return Response(json.dumps({"Erro":"Não foi possível retornar o usuário"}),
+                        status=404, mimetype='application/json')
     finally:
         session.close()
